d4e0e02f3811f573dea4eb895a725256a79f5c0c
Ahafonov-mykyta <ahafonovmykyta@gmail.com>
2022-07-06 13:42:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,11 +7,18 @@ const input = document.querySelector("input");
 const textarea = document.querySelector("textarea");
 
 
+
 const objectValues = {
-    email:"", 
+    email:"",
     message:"",
 };
 
+if(localStorage.getItem (LOCAL_DATA_INPUT, objectValues)) {
+
+    const parce = JSON.parse(localStorage.getItem (LOCAL_DATA_INPUT,objectValues))
+    objectValues.email = parce.email;
+    objectValues.message = parce.message
+    console.log(objectValues)}
 
 
 // Слушатель формы, на который делегируется события ввода инпутов
@@ -19,7 +26,7 @@ form.addEventListener("input", throttle(onInput, 500));
 
 // Функция записи данных ввода в объект локального хранилища 
 function onInput (event){
- 
+    
     if (event.target === input){
     objectValues.email = event.target.value;
     localStorage.setItem (LOCAL_DATA_INPUT,JSON.stringify(objectValues) )
@@ -37,23 +44,27 @@ function onInput (event){
 //Функция проверки наличия данных в хранилище 
 const storageAvailability = () =>{
 if(localStorage.getItem (LOCAL_DATA_INPUT, objectValues)){
-    const parcedObject = JSON.parse(localStorage.getItem (LOCAL_DATA_INPUT,objectValues.email));
-    input.value = parcedObject.email;}
+    const parcedObject = JSON.parse(localStorage.getItem (LOCAL_DATA_INPUT,objectValues));
+    input.value = parcedObject.email;
+    textarea.value = parcedObject.message;}}
 
-if(localStorage.getItem (LOCAL_DATA_INPUT, objectValues)){
-    const parcedObject = JSON.parse(localStorage.getItem (LOCAL_DATA_INPUT,objectValues.message));
-    textarea.value = parcedObject.message;}
-}
 
-    storageAvailability()
+storageAvailability()
 
 
 // Функция сабмита формы - очищает хранилище, поля ввода и выводит данные в консоль
 const onSubmit = (event) => {
     event.preventDefault();
+    if ( input.value === "" || textarea.value === ""){
+       return console.log("Ошибка")
+    }
+    
     console.log(`Email:${input.value}  Message:${textarea.value}`)
     form.reset()
     localStorage.removeItem(LOCAL_DATA_INPUT)
+    objectValues.email= "";
+    objectValues.message = "";
+    
 }
 
 form.addEventListener("submit", onSubmit)
